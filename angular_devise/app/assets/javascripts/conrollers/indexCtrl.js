1f120fb4292a699cb7d67782587ec1ec6ce46da6
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-03-17 12:19:30 -0400

    adding veggietables

--- angular_devise/app/assets/javascripts/conrollers/indexCtrl.js
+++ angular_devise/app/assets/javascripts/conrollers/indexCtrl.js
@@ -1,7 +1,8 @@
 (function (){
   "use strict";
   var farmApp = angular.module('farmApp')
-  .controller('indexCtrl', function($scope){
+  .controller('indexCtrl', function($scope)
+    
   .config(function($httpProvider) {
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $("meta[name=csrf-token]").attr("content");
      })
