a88760904cf6791e2aa40a60d503ea6231e61e93
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-03-25 09:08:41 -0400

    some progress

--- VeggieTables/VeggieTables/app/controllers/application_controller.rb
+++ VeggieTables/VeggieTables/app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ private
   end 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password, :password_confirmation, :roles[])} 
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password, :password_confirmation)} 
     devise_parameter_sanitizer.for(:account_update) << :admin
   end
     
